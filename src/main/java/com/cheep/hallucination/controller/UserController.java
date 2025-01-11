2efcffd82080059ddb5b2a988126882308114b33
park20011029 <rokmck156@gmail.com>
2025-01-11 21:22:04 +0900

    Test: test commit

--- src/main/java/com/cheep/hallucination/controller/UserController.java
+++ src/main/java/com/cheep/hallucination/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cheep.hallucination.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("")
+    public Boolean test(
+
+    ) {
+        return true;
+    }
 
 
 }
