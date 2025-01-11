292089107d520dcb841dd412f80852f11cf90264
park20011029 <rokmck156@gmail.com>
2025-01-11 17:31:07 +0900

    Fix: 레포지토리 명명 수정

--- src/main/java/com/cheep/hallucination/repository/CharacterRepository.java
+++ src/main/java/com/cheep/hallucination/repository/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.cheep.hallucination.repository;
 
+import com.cheep.hallucination.domain.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
---
+++ src/main/java/com/cheep/hallucination/repository/RoomInterviewRepository.java
@@ -0,0 +1,6 @@
+package com.cheep.hallucination.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomInterviewRepository extends JpaRepository<com.cheep.hallucination.domain.RoomInterview, Long> {
+}
